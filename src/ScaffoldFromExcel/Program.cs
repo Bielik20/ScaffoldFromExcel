2cb5cf8f2e113727920cb8664bb2d2362b441d6c
Bielik20 <bielik20@gmail.com>
2017-01-02 16:08:25 +0100

    Added console messages

--- src/ScaffoldFromExcel/Program.cs
+++ src/ScaffoldFromExcel/Program.cs
@@ -11,6 +11,13 @@ namespace ScaffoldFromExcel
     {
         public static void Main(string[] args)
         {
+            if (!File.Exists(AppContext.BaseDirectory + @"\input.xlsx"))
+            {
+                Console.WriteLine("You must place valid 'input.xlsx' file in program directory");
+                Console.Read();
+                return;
+            }
+
             var reader = new ExcelReader(AppContext.BaseDirectory + @"\input.xlsx");
             var modelList = reader.GetModelList();
             var writer = new Writer(modelList);
@@ -22,6 +29,12 @@ namespace ScaffoldFromExcel
                     writer.WriteFiles(Extensions.ReadFile(filePath), Path.GetFileName(filePath));
                 }
             }
+            else
+            {
+                Console.WriteLine("No Base Files detected.\n" + 
+                    "To add Base Files create folder in program directory named " +
+                    "'BaseFile' and drop your Base Files there");
+            }
 
             if (Directory.Exists(AppContext.BaseDirectory + @"\BaseLines"))
             {
@@ -30,6 +43,16 @@ namespace ScaffoldFromExcel
                     writer.WriteLines(Extensions.ReadFile(filePath), Path.GetFileName(filePath));
                 }
             }
+            else
+            {
+                Console.WriteLine("No Base Lines detected.\n" + 
+                    "To add Base Lines create folder in program directory named " + 
+                    "'BaseLines' and drop your Base Lines there");
+            }
+
+            Console.WriteLine("Scaffolding completed.");
+            Console.Read();
+            return;
         }
     }
 }
